2908b6327734e28f5f4b14c0da40598116ae2a64
Jonas Windmolders <jonas.windmolders@saga.be>
2017-01-09 17:57:31 +0100

    all dep in pi.js

--- client/src/pi.js
+++ client/src/pi.js
@@ -4,8 +4,19 @@
  * Load all external dependencies
  * We use var so it's global available : ) ( not let,const)
  */
+var fs      = require('fs');
+var _ = require('lodash');
 
 var Gopigo = require('./modules/node-gopigo');
+var Commands = Gopigo.commands;
+var Robot = Gopigo.robot;
+var readline = require('readline');
 
+/**
+ * Load all internal dependencies
+ */
+const cfg     = require('../config.js');
+var pkg     = require('../package.json');
+var piJS     = require('./modules/pi-js-module');
 
-//let pi = new piJS(cfg);
+let pi = new piJS(cfg);
